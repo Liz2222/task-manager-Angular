675592bcd3b4ade3da6c9fe4873e54f242906a26
Liz <adhiamboliz3@gmail.com>
2022-04-14 00:58:14 +0300

    designed frontend look with css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskviewComponent } from './pages/taskview/taskview.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskviewComponent
   ],
   imports: [
     BrowserModule,
